248165ade6827ffe5ca28a235f51e3a4686ca8aa
AnyaMankova <ao.mankova@yandex.ru>
2023-03-12 11:44:17 +1100

    update images

--- taxi_f.py
+++ taxi_f.py
@@ -14,16 +14,13 @@ uber_df['Hour'] = uber_df['Date/Time'].dt.hour
 uber_df['Day_of_week'] = uber_df['Date/Time'].dt.weekday
 
 
-# Делаем визуализацию распределения вызовов в зависимости от дня в месяце
-#создаю фигуру, на которой будет располагаться гистограмма
 fig = plt.figure(figsize=[12, 6])
 ax = fig.add_subplot()
 
-ax.hist(uber_df['Day'], bins= 30, color= "orange", edgecolor='black')
-ax.set_title('Распределение вызовов такси по датам месяца', fontsize=16)
-ax.set_xlabel('День', fontsize=14)
-ax.set_xticks([i for i in range(0,31)])
+ax.hist(uber_df['Hour'], bins=24, color= "orange", edgecolor='black')
+ax.set_title('Распределение вызовов такси в зависимости от времени суток', fontsize=16)
+ax.set_xlabel('Время суток', fontsize=14)
+ax.set_xticks([i for i in range(0,24)])
 ax.set_ylabel('Количество вызовов', fontsize=14)
-plt.savefig('images/Day.png', dpi=120)
-plt.show()
+plt.savefig('images/Hour.png', dpi=120)
 
